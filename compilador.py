9bbea1e270526489823ec44fa8c9df1085a72512
LeviCanales <milevi271@gmail.com>
2018-04-20 06:06:34 -0600

    Hasta antes de empezar a reconocer las asignaciones

--- compilador.py
+++ compilador.py
@@ -1,4 +1,5 @@
 import func
+import re
 
 #tuplas de terminales
 tipos = 'INT','FLOAT','DOUBLE','SHORT','BIGINT'
@@ -9,19 +10,32 @@ file = open("codigo_fuente.txt")
 #variables de trabajo
 original_file = file.read()
 work_file = func.clean_code(original_file, '\n',chr(9))
-print(work_file)
+#print(work_file)
 work_file = work_file.strip()
 state_code = ''
 try:
-    #esta bien la parte del 'DEFINE PROGRAM '?
+    #¿esta bien la parte del 'DEFINE PROGRAM '?
     if(work_file.startswith(otros[0])):
         state_code += '✔ '+ otros[0]
         work_file = work_file.lstrip(otros[0])
-        print(state_code)
     else:
         state_code += '✘ '+'"'+otros[0]+'"'+ ' No encontrado de forma correcta.'
         raise NameError(state_code)
+    #Ver si esta bien el patron number y el ()
+    work_file = work_file.split(otros[1],1)
+    work_file[0] = work_file[0].strip()
+    if re.search(r"^[a-zA-Z]+[0-9]*$",work_file[0]):
+        state_code += work_file[0]+ otros[1]
+        work_file = work_file[1].lstrip()
+        print(work_file)
+    else:
+        state_code = '✘ '+work_file[0]+' y '+ otros[1]+' No encontrado de forma correcta.'
+        raise NameError(state_code)
+    #Ver que haya un DO bien puesto xD
+    if re.search(r"(\)|;)( )*DO( )*(\$|W)",work_file):
+        print('DO')
 except:
     raise
 finally:
+    #print(", ".join(otros))
     file.close()
